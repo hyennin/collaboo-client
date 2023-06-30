446eb5cc0b41c4284b257cbdcb337f4f1627d6f5
hyennin <chm050202@gmail.com>
2023-06-30 12:38:45 +0900

    💄 feat: 프로젝트 생성 페이지 기본 UI 틀

--- src/pages/genProject.js
+++ src/pages/genProject.js
@@ -5,6 +5,26 @@ const GenProject = () => {
   return (
     <div>
       <Header title='프로젝트 생성'/>
+      <div>
+        <form>
+          <label>프로젝트명</label>
+          <input/>
+          <label>마감일</label>
+          <div>
+            <input/>
+            ~ 
+            <input/>
+          </div>
+          <label>인원</label>
+          <div>
+            <div></div>
+            <p>명</p>
+          </div>
+          <label>프로젝트 소개</label>
+          <textarea/>
+        </form>
+      </div>
+      <button>프로젝트 생성</button>
     </div>
   );
 }
