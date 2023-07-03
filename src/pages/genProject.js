f2d6e5e88436004ea0b876cfc0a35d39444d9bbe
hyennin <chm050202@gmail.com>
2023-07-03 11:52:07 +0900

    💄 feat: 프로젝트 생성 UI 구현

--- src/pages/genProject.js
+++ src/pages/genProject.js
@@ -1,30 +1,53 @@
+import { useState } from 'react';
 import Header from '../components/header';
 import styles from '../styles/GenProject.module.css';
+import down from '../assets/down.png';
 
 const GenProject = () => {
+  const [currentValue, setCurrentValue] = useState("2");
+  const [showOptions, setShowOptions] = useState(false);
+  const handleOnChangeSelectValue = (e) => {
+    setCurrentValue(e.target.getAttribute("value"));
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Header title='프로젝트 생성'/>
-      <div>
-        <form>
-          <label>프로젝트명</label>
-          <input/>
-          <label>마감일</label>
-          <div>
-            <input/>
-            ~ 
-            <input/>
+      <div className={styles.gen_container}>
+        <form className={styles.form}>
+          <label className={styles.label}>프로젝트명</label>
+          <input className={styles.input}/>
+          <label className={styles.label}>마감일</label>
+          <div className={styles.datepicker}>
+            <input className={styles.from}/>&nbsp;&nbsp;~&nbsp;&nbsp;<input className={styles.to}/>
           </div>
-          <label>인원</label>
-          <div>
-            <div></div>
+          <label className={styles.label}>인원</label>
+          <div className={styles.select_container}>
+            <div className={styles.select} onClick={() => {
+              console.log(showOptions)
+              setShowOptions(!showOptions)
+            }}>
+              <div className={showOptions ? styles.selected2 : styles.selected}>
+                <div className={styles.num}>{currentValue}</div>
+                <img className={styles.down_btn} src={down} alt='down'/>
+              </div>
+              <div className={styles.option_container} style={{ display: showOptions ? 'block' : 'none'}}>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='2'>2</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='3'>3</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='4'>4</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='5'>5</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='6'>6</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='7'>7</div>
+                <div className={styles.option} onClick={handleOnChangeSelectValue} value='8'>8</div>
+              </div>
+            </div>
             <p>명</p>
           </div>
-          <label>프로젝트 소개</label>
-          <textarea/>
+          <label className={styles.label}>프로젝트 소개</label>
+          <textarea className={styles.info}/>
         </form>
       </div>
-      <button>프로젝트 생성</button>
+      <button className={styles.gen_btn}>프로젝트 생성</button>
     </div>
   );
 }
